c92ca8c7989bb098e3bcc6b6a130428dd6e8f914
matan.kaufman <matankaufman1@gmail.com>
2024-12-30 18:42:56 +0200

    update file

---
+++ terraform/vpc.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git?ref=f02a1af5aedc550c81048cfa880153bedf2a006d"
+  version = "5.15.0"
+
+  name = "circleci-vpc"
+
+  cidr = "172.20.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["172.20.1.0/24", "172.20.2.0/24", "172.20.3.0/24"]
+  public_subnets  = ["172.20.4.0/24", "172.20.5.0/24", "172.20.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+}
