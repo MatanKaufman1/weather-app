2696e27d3004c0e62f50305c581d82b238eb0369
matan.kaufman <matankaufman1@gmail.com>
2025-01-01 14:48:20 +0200

    update selenium-test

--- src/tests/test-selenium.py
+++ src/tests/test-selenium.py
@@ -1,14 +1,11 @@
-# import all required frameworks
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
-# inherit TestCase Class and create a new test class
 class PythonOrgSearch(unittest.TestCase):
 
-    # initialization of webdriver
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.url = "http://localhost:5000"
@@ -26,13 +23,8 @@ class PythonOrgSearch(unittest.TestCase):
     def test_negative(self):
         pass
 
-
-
-
     def tearDown(self):
         self.driver.close()
 
-
-# execute the script
 if __name__ == "__main__":
     unittest.main()
